bd936e3bbc46a3fb6516097afbea764fab30d2ba
Kacper <kacper.jedrzejewski@int.pl>
2020-09-29 20:58:00 +0200

    Add project files.

---
+++ ScienceBook.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ScienceBook.Web.Startup))]
+namespace ScienceBook.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
